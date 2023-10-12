b1ccb94a7d818613bc0b94a8f6d1003af9a3c6a8
Terry Chitter <chittert@gmail.com>
2023-10-12 18:38:19 +0200

    add payment functionality with php

---
+++ dashboard/add_payment.php
@@ -0,0 +1,109 @@
+<?php
+// Establish a database connection
+require_once("../db_conn.php");
+
+// Get customer id, amount, date, and type
+$accountNumber = $_GET['customer_id'];
+$paymentAmount = 100;
+$paymentDate = date('2023-10-11') . ' ' . date('H:i:s');
+$paymentType = "EFT";
+
+// Get the most recent balance
+$sql_current_balance = "SELECT balance_amount FROM Balances WHERE customer_id = $accountNumber ORDER BY balance_date DESC LIMIT 1";
+$results_current_balance = mysqli_query($conn, $sql_current_balance);
+
+if ($results_current_balance) {
+    $row = mysqli_fetch_assoc($results_current_balance);
+    $currentBalance = $row['balance_amount'];
+}
+
+echo "Current Balance: $currentBalance";
+
+// Calculate the new balance
+$updatedBalance = $currentBalance - $paymentAmount;
+
+// Generate the payment id
+$uniquePaymentID = generateUniquePaymentID($accountNumber, $paymentDate, $paymentType, $conn);
+
+
+// Add a record of the payment
+if (addPayment($uniquePaymentID, $accountNumber, $paymentDate, $paymentAmount, $paymentType, $conn)) {
+    // Add a record of the updated balance
+    if (addBalance($accountNumber, $paymentDate, $updatedBalance, $uniquePaymentID, $conn)) {
+        // Both the payment and balance records were added successfully
+        // Commit the transaction
+        mysqli_commit($conn);
+        echo "Payment and balance records added successfully.<br>";
+    } else {
+        // Error adding balance record
+        // Roll back the transaction
+        mysqli_rollback($conn);
+        echo "Error adding balance record.<br>";
+    }
+} else {
+    // Error adding payment record
+    // Roll back the transaction
+    mysqli_rollback($conn);
+    echo "Error adding payment record.<br>";
+}
+
+// Function to add a payment record
+function addPayment($uniquePaymentID, $accountNumber, $paymentDate, $paymentAmount, $paymentType, $conn)
+{
+    $sql = "INSERT INTO payments (payment_id, customer_id, payment_date, payment_amount, payment_type)
+            VALUES ('$uniquePaymentID', '$accountNumber', '$paymentDate', $paymentAmount, '$paymentType')";
+
+    return mysqli_query($conn, $sql);
+}
+
+// Function to add a balance record
+function addBalance($accountNumber, $paymentDate, $updatedBalance, $uniquePaymentID, $conn)
+{
+
+    $sql = "INSERT INTO balances (customer_id, balance_date, balance_amount, payment_id)
+            VALUES ('$accountNumber', '$paymentDate', $updatedBalance, '$uniquePaymentID')";
+
+    return mysqli_query($conn, $sql);
+}
+
+function generateUniquePaymentID($accountNumber, $paymentDate, $paymentType, $conn)
+{
+    // Extract year, month, and day from the payment date
+    $year = date('y', strtotime($paymentDate));
+    $month = date('m', strtotime($paymentDate));
+    $day = date('d', strtotime($paymentDate));
+
+    // Extract the first two letters of the payment type (uppercase)
+    $paymentType = strtoupper(substr($paymentType, 0, 2));
+
+    // Initialize a counter to handle duplicates
+    $counter = 1;
+
+    // Construct the initial payment ID
+    $paymentID = "PMT{$year}{$month}{$day}{$paymentType}{$accountNumber}";
+
+    // Check if the initial payment ID already exists in the database
+    $uniquePaymentID = $paymentID;
+    while (paymentIDExists($uniquePaymentID, $conn)) {
+        $counter++;
+        $uniquePaymentID = "PMT{$year}{$month}{$day}{$paymentType}{$accountNumber}-$counter";
+    }
+
+    return $uniquePaymentID;
+}
+
+// Function to check if a payment ID already exists in the payments table
+function paymentIDExists($paymentID, $conn)
+{
+    $sql = "SELECT COUNT(*) AS count FROM payments WHERE payment_id = '$paymentID'";
+    $result = mysqli_query($conn, $sql);
+
+    if ($result) {
+        $row = mysqli_fetch_assoc($result);
+        return $row['count'] > 0;
+    }
+
+    return false;
+}
+
+?>
