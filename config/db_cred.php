876c188774b1f36a437038ac347f8c07f6746f87
Terry Chitter <chittert@gmail.com>
2024-02-04 14:28:49 +0200

    add config file

---
+++ config/db_cred.php
@@ -0,0 +1,5 @@
+<?php
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "cms_db";
---
+++ config/keys.php
@@ -0,0 +1,3 @@
+<?php
+$recaptchaKey = 'your_secret_recaptcha_key';
+$secretLoginToken = "12345678";
